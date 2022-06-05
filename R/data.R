4aa42c6bb47ab9a1ed0ffd7f6abb597b3f9cf98e
AparicioJohan <johan.aparicio@correounivalle.edu.co>
2022-06-05 09:39:31 -0500

    docs: added example data documentation

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Example data.
+#'
+#' A dataset containing an alpha lattice design.
+#'
+#' @format A data frame with 792 rows and 18 columns:
+#' @source \url{CIAT: Bean Breeding Program}
+"Dar16C_hiP"
